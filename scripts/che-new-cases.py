fa265a1fadf36d6d954f07581ede0f8f84bcff8e
PeterR <piotrrutkowski97@gmail.com>
2020-05-01 18:26:53 +0200

    up

--- scripts/che-new-cases.py
+++ scripts/che-new-cases.py
@@ -2,28 +2,20 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-from  utils import drop_prepandemic_dates
+from  utils import drop_prepandemic_dates, save_ch_neighbours
+save_ch_neighbours()
 
-
-data = pd.read_csv('switzerland.csv')
+data = pd.read_csv('che.csv')
 data = data[['date','new_cases']]
 
 data = drop_prepandemic_dates(data)
+dates = data['date']
 
 sns.set(style="whitegrid")
 
 fig, ax = plt.subplots()
-
-xticks = np.arange(0,64,10)
-dates = []
-for index, row in data.iterrows():
-    print(index)
-    if (index-59) in xticks:
-        dates.append(row['date'])
-print(dates)
-
 plot = sns.barplot(x='date', y='new_cases', data=data, ax=ax)
-plot.set(xticks=xticks, xticklabels=['28Feb','9Mar','19Mar','29Mar','8Apr','18Apr','28Apr'],
+plot.set(xticks=np.arange(0,len(data['date']),15), xticklabels=[dates[ind] for ind in range(0,len(data['date']),15)],
          xlabel='Date', ylabel='New daily cases', title='New daily cases of COVID-19 in Switzerland')
 
 plot = ax.get_figure()
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,23 +1,37 @@
 import pandas as pd
 
-def save_ch_neighbours(filename='ourworldindatadeathetc.csv'):
-    df = pd.read_csv(filename)
-    df.loc[df['location'] == 'Switzerland'].to_csv('switzerland.csv')
-    df.loc[df['location'] == 'France'].to_csv('france.csv')
-    df.loc[df['location'] == 'Italy'].to_csv('italy.csv')
-    df.loc[df['location'] == 'Germany'].to_csv('germany.csv')
-    df.loc[df['location'] == 'Austria'].to_csv('austria.csv')
-    df.loc[df['location'] == 'Liechtenstein'].to_csv('liechtenstein.csv')
-
-def load_ch_neighbours():
-    return pd.read_csv('switzerland.csv'), pd.read_csv('france.csv'),\
-           pd.read_csv('italy.csv'), pd.read_csv('germany.csv'),\
-           pd.read_csv('austria.csv'), pd.read_csv('liechtenstein.csv')
-
 def drop_prepandemic_dates(data):
     prepandemic_dates = []
     for index, row in data.iterrows():
-        prepandemic_dates.append((index))
         if row['new_cases'] > 0:
             break
+        prepandemic_dates.append((index))
     return data.drop(labels=prepandemic_dates)
+
+def convert_dates(data):
+    months = {'01':'Jan','02':'Feb','03':'Mar','04':'Apr','05':'May','06':'Jun',
+              '07':'Jul','08':'Aug','09':'Sep','10':'Oct','11':'Nov','12':'Dec'}
+    for index, row in data.iterrows():
+        data.at[index,'date'] = row['date'][-2:] + months[row['date'][5:7]]
+    return data
+
+def save_ch_neighbours(filename='ourworldindatadeathetc.csv'):
+    df = pd.read_csv(filename)
+    che = df.loc[df['location'] == 'Switzerland']
+    fra = df.loc[df['location'] == 'France']
+    ita = df.loc[df['location'] == 'Italy']
+    ger = df.loc[df['location'] == 'Germany']
+    aut = df.loc[df['location'] == 'Austria']
+    lie = df.loc[df['location'] == 'Liechtenstein']
+
+    countries = [che, fra, ita, ger, aut, lie]
+    codes = ['che', 'fra', 'ita', 'ger', 'aut', 'lie']
+
+    for i in range(len(countries)):
+        convert_dates(drop_prepandemic_dates(countries[i])).to_csv('%s.csv'%(codes[i]))
+
+def load_ch_neighbours():
+    return pd.read_csv('che.csv'), pd.read_csv('fra.csv'),\
+           pd.read_csv('ita.csv'), pd.read_csv('ger.csv'),\
+           pd.read_csv('aut.csv'), pd.read_csv('lie.csv')
+
